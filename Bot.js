3656b4da546ea643a489623e7244fc4d9fc1db4a
Jeremy M Rivera <fangman09@gmail.com>
2023-04-28 17:52:45 -0500

    need something to hover

--- Bot.js
+++ Bot.js
@@ -8,11 +8,16 @@ const args = [
   '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
 ];
 
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
+puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
 
 const product_url =
-  'https://www.walmart.com/ip/Lavalier-Clip-on-Lapel-Omnidirectional-Condenser-Microphone-For-SmartPhone/969327603?from=searchResults';
-
+  'https://creations.mattel.com/pages/hot-wheels-collectors-membership';
 async function givePage() {
   const browser = await puppeteer.launch({
     headless: false,
@@ -24,10 +29,27 @@ async function givePage() {
 
 async function addToCart(page) {
   await page.goto(product_url);
+
+  await page.waitForSelector(
+    'a[class="button button--primary hero-banner-add-to-cart submit-add-to-cart white-bg-cta"]'
+  );
+  await page.click(
+    'a[class="button button--primary hero-banner-add-to-cart submit-add-to-cart white-bg-cta"]'
+  );
+  //   await page.waitForNavigation();
+  await page.waitForSelector('label[ class="accept-terms-conditions_label"]');
+  await page.click('label[ class="accept-terms-conditions_label"]');
+
+  await page.waitForSelector(
+    'button[class="cart__checkout-button button button--primary black-bg-cta"]'
+  );
+  await page.click(
+    'button[class="cart__checkout-button button button--primary black-bg-cta"]'
+  );
 }
 
 async function checkout() {
-  let page = await givePage();
+  var page = await givePage();
   await addToCart(page);
 }
 checkout();
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-### BotDevelopment
+# Bot Development
 
 The purpose of this project was to continue practicing my Node JS skills for backend development. I also wanted to test out the Library Puppeteer for experience building things without a front end UI. I also had come across the idea because a family member enjoys purchasing rare and limited collectors items and every time I wait in a virtual line the prodect is immediately sold out, due to other 'sneaker' or 'checkout' bots.
 So I thought it would be a nice personal side project to build one.
+
+## Ideas Only
+
+Again not for commercial use, anyone seeing this may want to use it but if used incorrectly, or even correctly, it may get your IP Address banned from whatever site you're testing this from.
+For example, I initially tested this software on Walmart.com but they kept throwing 'captchas' or other mouse tricks and even when I jumped and fulfilled the tests correctly it jumped off. I later tested the software on Creation.Hasbro.com but what was interesting was that the element doesn't pop up on screen (according to the same ID) until the mouse tracks over it.
