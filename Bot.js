3fb1f1c083f7dcd3227d6f42e0197af2ff8b718a
Jeremy M Rivera <fangman09@gmail.com>
2023-04-28 16:46:03 -0500

    added args

--- Bot.js
+++ Bot.js
@@ -1,12 +1,33 @@
+const args = [
+  '--no-sandbox',
+  '--disable-setuid-sandbox',
+  '--disable-infobars',
+  '--window-position=0,0',
+  '--ignore-certificate-errors',
+  '--ignore-certificate-errors-spki-list',
+  '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
+];
+
 const puppeteer = require('puppeteer');
 
 const product_url =
   'https://www.walmart.com/ip/Lavalier-Clip-on-Lapel-Omnidirectional-Condenser-Microphone-For-SmartPhone/969327603?from=searchResults';
 
-async function createPage() {
-  const browser = await puppeteer.launch({ headless: true });
+async function givePage() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: args,
+  });
   const page = await browser.newPage();
-  await page.goto(product_url);
-  console.log('done');
+  return page;
 }
-createPage();
+
+async function addToCart(page) {
+  await page.goto(product_url);
+}
+
+async function checkout() {
+  let page = await givePage();
+  await addToCart(page);
+}
+checkout();
