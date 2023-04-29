a5e2a49d617c63ff1c0dba63832eff3d187babb2
Jeremy M Rivera <fangman09@gmail.com>
2023-04-28 19:02:09 -0500

    fill billing updated and not working

--- Bot.js
+++ Bot.js
@@ -37,6 +37,7 @@ async function addToCart(page) {
   await page.click(
     'a[class="button button--primary hero-banner-add-to-cart submit-add-to-cart white-bg-cta"]'
   );
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
   await page.waitForSelector('label[ class="accept-terms-conditions_label"]');
   await page.click('label[ class="accept-terms-conditions_label"]');
@@ -47,20 +48,22 @@ async function addToCart(page) {
   await page.click(
     'button[class="cart__checkout-button button button--primary black-bg-cta"]'
   );
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
 }
 
 async function fillBilling(page) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  await page.type("input[id='checkout_email'}", 'test1@gmail.com');
-  await page.type("input[id='checkout_billing_address_first_name'}", 'John');
-  await page.type("checkout_billing_address_last_name'}", 'Doe');
+  await page.type("input[id='checkout_email']", 'test1@gmail.com');
+  await page.type("input[id='checkout_billing_address_first_name']", 'John');
+  await page.type("input[id='checkout_billing_address_last_name']", 'Doe');
   await page.type(
-    "input[id='checkout_billing_address_address1'}",
+    "input[id='checkout_billing_address_address1']",
     '30419 Double Dr'
   );
-  await page.type("select[id='checkout_billing_address_province'}", 'Florida');
-  await page.type("input[id='checkout_billing_address_zip'}", '33544');
+  await page.type("select[id='checkout_billing_address_province']", 'Florida');
+  await page.type("input[id='checkout_billing_address_zip']", '33544');
 }
+
 async function checkout() {
   var page = await givePage();
   await addToCart(page);
