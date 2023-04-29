6fcd70871168b824ba48ac9f025b2d820ec5927d
Jeremy M Rivera <fangman09@gmail.com>
2023-04-28 19:00:23 -0500

    rework it more

--- Bot.js
+++ Bot.js
@@ -33,10 +33,11 @@ async function addToCart(page) {
   await page.waitForSelector(
     'a[class="button button--primary hero-banner-add-to-cart submit-add-to-cart white-bg-cta"]'
   );
+
   await page.click(
     'a[class="button button--primary hero-banner-add-to-cart submit-add-to-cart white-bg-cta"]'
   );
-  //   await page.waitForNavigation();
+
   await page.waitForSelector('label[ class="accept-terms-conditions_label"]');
   await page.click('label[ class="accept-terms-conditions_label"]');
 
@@ -48,8 +49,27 @@ async function addToCart(page) {
   );
 }
 
+async function fillBilling(page) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await page.type("input[id='checkout_email'}", 'test1@gmail.com');
+  await page.type("input[id='checkout_billing_address_first_name'}", 'John');
+  await page.type("checkout_billing_address_last_name'}", 'Doe');
+  await page.type(
+    "input[id='checkout_billing_address_address1'}",
+    '30419 Double Dr'
+  );
+  await page.type("select[id='checkout_billing_address_province'}", 'Florida');
+  await page.type("input[id='checkout_billing_address_zip'}", '33544');
+}
 async function checkout() {
   var page = await givePage();
   await addToCart(page);
+  await fillBilling(page);
 }
 checkout();
+
+// This stuff may be beneficial for the future
+//   await page.waitForNavigation();
+//   await page.hover(
+//     'a[class="button button--primary hero-banner-add-to-cart submit-add-to-cart white-bg-cta"]'
+//   );
